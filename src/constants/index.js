5a94cbd533228ad71e6f6b58343cf1d94e3a3f7c
github username <github email address>
2019-03-17 18:19:35 -0400

    backend

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_ROOT = 'http://localhost:3001';
+export const API_ROOT = 'https://politic-backend.herokuapp.com/';
 export const API_WS_ROOT = 'ws://localhost:3001/cable?token=';
 export const HEADERS = {
   'Content-Type': 'application/json',
