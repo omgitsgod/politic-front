1fa203f3822391f29eb3906a4090d65bc45e6bae
Ryan <omgitsgod@gmail.com>
2020-06-28 23:04:28 -0400

    Refactor and fix formatting

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,87 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import FormControl from '@material-ui/core/FormControl';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
+import { Avatar, Button, FormControl, FormControlLabel, Checkbox, Input, InputLabel, Paper, Typography } from '@material-ui/core';
 import LockIcon from '@material-ui/icons/LockOutlined';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
 import withStyles from '@material-ui/core/styles/withStyles';
 
-const styles = theme => ({
-  main: {
-    width: 'auto',
-    display: 'block', // Fix IE 11 issue.
-    marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
-    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
-      width: 400,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    },
-  },
-  paper: {
-    marginTop: theme.spacing.unit * 8,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
-  },
-  avatar: {
-    margin: theme.spacing.unit,
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing.unit,
-  },
-  submit: {
-    marginTop: theme.spacing.unit * 3,
-  },
-});
 
-class SignIn extends Component  {
+function SignIn(props) {
 
-  handleChange = ({ target: { name, value } }) =>
-
-
-    this.setState({
-      [name]: value
-    })
-
-
-  actionTime = (e) => {
-    e.preventDefault()
-
+  const { classes, handleUser } = props;
+  
+  const handleAction = async (e) => {
+    e.preventDefault();
 
     const user = {
       username: e.target.username.value,
       password: e.target.password.value
     }
+    const json = await fetch('https://politic-backend.herokuapp.com/api/v1/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json'
+        },
+        body: JSON.stringify({
+          user
+        })
+      })
+      .then(r => r.json());
 
-    fetch('https://politic-backend.herokuapp.com/api/v1/login', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    Accept: 'application/json'
-  },
-  body: JSON.stringify({
-    user
-  })
-})
-  .then(r => r.json())
-  .then(json => this.props.handleUser(json))
+    handleUser(json);
   }
 
-  render() {
-    const { classes } = this.props;
   return (
     <main className={classes.main}>
-
       <Paper className={classes.paper}style={{ background: 'transparent', boxShadow: 'none'}}>
         <Avatar  className={classes.avatar}>
           <LockIcon />
@@ -91,9 +42,9 @@ class SignIn extends Component  {
         </Typography>
         <form
          className={classes.form}
-         onSubmit={this.actionTime}
+         onSubmit={handleAction}
          autoComplete="off"
-         >
+        >
           <FormControl margin="normal" required fullWidth >
             <InputLabel htmlFor="username">Username</InputLabel>
             <Input id="username" name="username"  autoFocus  />
@@ -119,11 +70,44 @@ class SignIn extends Component  {
         </form>
       </Paper>
     </main>
-  ); }
+  ); 
 }
 
 SignIn.propTypes = {
   classes: PropTypes.object.isRequired,
 };
+
+const styles = theme => ({
+
+  main: {
+    width: 'auto',
+    display: 'block',
+    marginLeft: theme.spacing(3),
+    marginRight: theme.spacing(3),
+    [theme.breakpoints.up(400 + theme.spacing(3 * 2))]: {
+      width: 400,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: `${theme.spacing(2)}px ${theme.spacing(3)}px ${theme.spacing(3)}px`,
+  },
+  avatar: {
+    margin: theme.spacing(),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(),
+  },
+  submit: {
+    marginTop: theme.spacing(3),
+  },
+});
 
 export default withStyles(styles)(SignIn);
