59a1412387f1bcda37bb0536eccbd43dcb9df075
Ryan <omgitsgod@gmail.com>
2020-07-03 18:53:44 -0400

    Refactor and fix formatting

--- src/VoteCard.js
+++ src/VoteCard.js
@@ -1,105 +1,99 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
+import { Card, CardActionArea, CardContent, Typography } from '@material-ui/core';
 
-const styles = (theme) => ({
-  card: {
-    maxWidth: 345,
-     marginTop: theme.spacing.unit * 8
-  },
-  media: {
-    height: 200,
-  },
-
-
-});
 
 function VoteCard(props) {
-  const { classes } = props;
+
+  const { classes, vote } = props;
+
   return (
-
     <Card className={classes.card}
-    style={{ opacity: '.7', boxShadow: 'none'}}
-    raised='true'>
+      style={{ opacity: '.7', boxShadow: 'none'}}
+      raised='true'
+    >
+      <CardActionArea>
+        <CardContent>
+          {(vote.url) ?
+            <a target='blank' href={vote.url} style={{ textDecoration: 'none', color: 'white' }}>
+              <Typography gutterBottom variant='title' component='p'>
+                Bill:  {vote.bill.title}
+              </Typography>
+              <Typography gutterBottom variant='title' component='p'>
+                {vote.question}?
+              </Typography>
 
-    <CardActionArea>
-    <CardContent>
-    {(props.vote.url) ?
-    <a target='blank' href={props.vote.url} style={{ textDecoration: 'none', color: 'white' }}>
-    <Typography gutterBottom variant="title" component="p">
-    Bill:  {props.vote.bill.title}
-    </Typography>
-    <Typography gutterBottom variant="title" component="p">
-      {props.vote.question}?
-    </Typography>
-
-    </a>
-    :
-    <div>
-    <Typography gutterBottom variant="title" component="p">
-    Bill:  {props.vote.bill.title}
-    </Typography>
-    <Typography gutterBottom variant="title" component="p">
-      {props.vote.question}?
-    </Typography>
-    </div>
-  }
-    </CardContent>
-    <br />
-<CardContent >
-{(props.vote.democratic) ?
-  <div>
-<Typography component="p" align='right' color='secondary'>
-      Democrates Voted: {props.vote.democratic.majority_position}
-</Typography>
-<Typography component="p" align='right' color='error'>
-    Republicans Voted: {props.vote.republican.majority_position}
-</Typography>
-</div>
-:
-''
-}{(props.vote.position) ?
-<Typography variant="h6" align='right' color="secondary" >
-I voted {props.vote.position}
-</Typography>
-:
-null
-}
-
-<Typography variant="h5" align='right'  style={{color: 'green'}}>
-Yay: {props.vote.total.yes}
-</Typography>
-<br />
-    <Typography variant="h5" align='right' color="error">
-    Nay: {props.vote.total.no}
-    </Typography>
-    <br />
-
-  {  (props.vote.result === "Passed") || (props.vote.result === "Cloture on the Motion to Proceed Passed") || ("Motion to Proceed Agreed to")?
-    <Typography variant="h5" align='center' color="secondary" >
-      {props.vote.result}
-    </Typography>
-    :
-    <Typography variant="h5" align='center' color="error" >
-        {props.vote.result}
-    </Typography>
-  }
-
-  <br />
-    </CardContent>
-    </CardActionArea>
-
+            </a>
+          :
+            <div>
+              <Typography gutterBottom variant='title' component='p'>
+                Bill:  {vote.bill.title}
+              </Typography>
+              <Typography gutterBottom variant='title' component='p'>
+                {vote.question}?
+              </Typography>
+            </div>
+          }
+        </CardContent>
+        <br />
+        <CardContent >
+          {(vote.democratic) ?
+            <div>
+              <Typography component='p' align='right' color='secondary'>
+                    Democrates Voted: {vote.democratic.majority_position}
+              </Typography>
+              <Typography component='p' align='right' color='error'>
+                  Republicans Voted: {vote.republican.majority_position}
+              </Typography>
+            </div>
+          :
+            ''
+          }
+          {(vote.position) ?
+            <Typography variant='h6' align='right' color='secondary' >
+              I voted {vote.position}
+            </Typography>
+          :
+            null
+          }
+          <Typography variant='h5' align='right'  style={{color: 'green'}}>
+            Yay: {vote.total.yes}
+          </Typography>
+          <br />
+          <Typography variant='h5' align='right' color='error'>
+            Nay: {vote.total.no}
+          </Typography>
+          <br />
+          {(vote.result === 'Passed') || (vote.result === 'Cloture on the Motion to Proceed Passed') || ('Motion to Proceed Agreed to') ?
+            <Typography variant='h5' align='center' color='secondary' >
+              {vote.result}
+            </Typography>
+          :
+            <Typography variant='h5' align='center' color='error' >
+              {vote.result}
+            </Typography>
+          }
+          <br />
+        </CardContent>
+      </CardActionArea>
     </Card>
-
   );
 }
 
 VoteCard.propTypes = {
   classes: PropTypes.object.isRequired,
 };
+
+const styles = (theme) => ({
+
+  card: {
+    maxWidth: 345,
+    marginTop: theme.spacing(8)
+  },
+  media: {
+    height: 200,
+  },
+});
 
 export default withStyles(styles)(VoteCard);
