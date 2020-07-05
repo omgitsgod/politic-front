4b554dba783143ed1ee7643d77c0448f7c014b09
Ryan <omgitsgod@gmail.com>
2020-07-05 11:53:49 -0400

    Refactor and fix formatting

--- src/IndusTable.js
+++ src/IndusTable.js
@@ -1,57 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-
-const styles = theme => ({
-  root: {
-    width: '100%',
-    marginTop: theme.spacing.unit * 3,
-    overflowX: 'auto',
-  },
-  table: {
-    minWidth: 700,
-  },
-});
-
-let id = 0;
-function createData(name, total, individual, pac) {
-  id += 1;
-  return { id, name, total, individual, pac};
-}
+import { Table, TableBody, TableCell, TableHead, TableRow, Paper } from '@material-ui/core';
 
 
+function IndusTable(props) {
 
+  const { classes, industry } = props;
+  const rows = industry.map(industry => createData(industry['@attributes'].industry_name, `$${industry['@attributes'].total}`, `$${industry['@attributes'].indivs}`, `$${industry['@attributes'].pacs}` ));
+  let id = 0;
 
+  const createData = (name, total, individual, pac) => {
 
-function SimpleTable(props) {
-  const { classes } = props;
-  const rows = props.industry.map(industry => createData(industry["@attributes"].industry_name, `$${industry["@attributes"].total}`, `$${industry["@attributes"].indivs}`, `$${industry["@attributes"].pacs}` ))
+    id += 1;
+    return { id, name, total, individual, pac };
+  }
+  
   return (
     <Paper className={classes.root} style={{ opacity: '.7', boxShadow: 'none'}}>
       <Table className={classes.table}>
         <TableHead>
           <TableRow>
             <TableCell>Industry</TableCell>
-            <TableCell align="right">Total</TableCell>
-            <TableCell align="right">Individuals</TableCell>
-            <TableCell align="right">PACS</TableCell>
+            <TableCell align='right'>Total</TableCell>
+            <TableCell align='right'>Individuals</TableCell>
+            <TableCell align='right'>PACS</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map(row => (
             <TableRow key={row.id}>
-              <TableCell component="th" scope="row">
+              <TableCell component='th' scope='row'>
                 {row.name}
               </TableCell>
-              <TableCell align="right">{row.total}</TableCell>
-              <TableCell align="right">{row.individual}</TableCell>
-              <TableCell align="right">{row.pac}</TableCell>
+              <TableCell align='right'>{row.total}</TableCell>
+              <TableCell align='right'>{row.individual}</TableCell>
+              <TableCell align='right'>{row.pac}</TableCell>
             </TableRow>
           ))}
         </TableBody>
@@ -60,8 +44,20 @@ function SimpleTable(props) {
   );
 }
 
-SimpleTable.propTypes = {
+IndusTable.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(SimpleTable);
+const styles = theme => ({
+
+  root: {
+    width: '100%',
+    marginTop: theme.spacing(3),
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 700,
+  },
+});
+
+export default withStyles(styles)(IndusTable);
