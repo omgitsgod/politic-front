a33cc147f6564c3f76146cd7705d83547ce5e4b7
Ryan <omgitsgod@gmail.com>
2020-07-06 13:14:21 -0400

    Refactor and fix formatting

--- src/Events.js
+++ src/Events.js
@@ -1,120 +1,107 @@
-import React from 'react';
-import { Component } from 'react'
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper'
-import { Tab, Tabs, Grid } from '@material-ui/core'
-import EventCard from './EventCard'
-import Pol2 from './Pol2'
-import { isBrowser} from "react-device-detect"
-
-import { Info } from '@material-ui/icons'
-
-const styles = (theme) => ({
-
-    main: {
-      width: 'auto',
-      display: 'block', // Fix IE 11 issue.
-      marginLeft: theme.spacing.unit * 3,
-      marginRight: theme.spacing.unit * 3,
-      [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
-        width: 1000,
-        marginLeft: 'auto',
-        marginRight: 'auto',
-      },
-    },
-    paper: {
-      marginTop: theme.spacing.unit * 8,
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
-    },
-  card: {
-    maxWidth: 345,
-     marginTop: theme.spacing.unit * 8
-  },
-  media: {
-    height: 200,
-  },
+import { Tab, Tabs, Grid, Paper } from '@material-ui/core';
+import { isBrowser} from 'react-device-detect';
+import { Info } from '@material-ui/icons';
+import EventCard from './EventCard';
+import Pol2 from './Pol2';
 
 
-});
+function Events(props) {
 
-class Events extends Component {
+  const [events, setEvents] = useState([]);
+  const [tab, setTab] = useState('Recent');
+  const [fed, setFed] = useState('');
+  const {classes} = props;
+  const gridNum = isBrowser ? 3 : 12;
+  const eventCards = events ? events.map(event => 
+    <Grid item xs={gridNum}> 
+      <EventCard event={event}/> 
+    </Grid>
+  ) : []
 
-  state = {
-    events : [],
-    tab: 'Recent',
-    fed: ''
+  const handlePol = (pol) => {
+
+    (pol === 'Back') ? setFed('') : setFed(pol);
   }
 
-  componentDidMount() {
-    fetch(`https://cors-anywhere.herokuapp.com/http://politicalpartytime.org/api/v1/event/?start_date__gt=2018-01-01&format=json`)
-    .then(r => r.json()).then(events => this.setState({events: events.objects}))
+  const handleChange = (event, change) => {
+
+    if (change !== 'Back') setTab(change);
   }
 
-  handlePol = (pol) => {
-    if (pol === "Back") {
-      this.setState({fed: ''})
-    } else {
-  this.setState({fed: pol})
-  }
-  }
+  const fetchEvents = async () => {
+    
+    const json = await fetch(`${process.env.REACT_APP_BACK_HOST}/events`).then(r => r.json());
 
-  handleChange = (event, change) => {
-    if (change === "Back") {
-    //  this.props.handlePol("Back")
-    } else {
-    this.setState({
-      tab: change
-    })
-  }
+    setEvents(json.objects)
   }
 
-  render() {
-      const { classes } = this.props;
-      let gridNum
-      if (isBrowser) {
-        gridNum = 3
-      } else {
-        gridNum = 12
-      }
-      const x = this.state.events.map(event => <Grid item xs={gridNum}> <EventCard event={event}/> </Grid>)
-    return (
-      <main className={classes.main}>
-      { (this.state.fed.length === 0) ?
-      <Paper className={classes.paper} style={{background: 'transparent', boxShadow: 'none'}}>
-      <Tabs
-        value={this.state.tab}
-       onChange={this.handleChange}
-        variant="fullWidth"
-        indicatorColor="secondary"
-        textColor="secondary"
-      >
+  useEffect(() => {
 
-        <Tab icon={<Info />} value={"Recent"} label="Most Recent Events" />
+    fetchEvents();
+  }, []);
 
-
-      </Tabs>
-      <Grid container spacing={16}>
-      <Grid container spacing={32} justify='center'>
-      {x}
-      </Grid>
-      </Grid>
-      </Paper>
+  return (
+    <main className={classes.main}>
+      {(fed.length === 0) ?
+        <Paper className={classes.paper} style={{background: 'transparent', boxShadow: 'none'}}>
+          <Tabs
+            value={tab}
+            onChange={handleChange}
+            variant='fullWidth'
+            indicatorColor='secondary'
+            textColor='secondary'
+          >
+            <Tab icon={<Info />} value={'Recent'} label='Most Recent Events' />
+          </Tabs>
+          <Grid container spacing={16}>
+            <Grid container spacing={32} justify='center'>
+              {eventCards}
+            </Grid>
+          </Grid>
+        </Paper>
       :
-      <Paper>
-      <Pol2 id={this.state.fed} handlePol={this.handlePol}/>
-      </Paper>
-    }
-      </main>
-    )
-  }
+        <Paper>
+          <Pol2 id={fed} handlePol={handlePol}/>
+        </Paper>
+      }
+    </main>
+  );
 }
 
 Events.propTypes = {
   classes: PropTypes.object.isRequired,
 };
+
+const styles = (theme) => ({
+
+  main: {
+    width: 'auto',
+    display: 'block',
+    marginLeft: theme.spacing(3),
+    marginRight: theme.spacing(3),
+    [theme.breakpoints.up(400 + theme.spacing(3 * 2))]: {
+      width: 1000,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: `${theme.spacing(2)}px ${theme.spacing(3)}px ${theme.spacing(3)}px`,
+  },
+  card: {
+    maxWidth: 345,
+    marginTop: theme.spacing(8)
+  },
+  media: {
+    height: 200,
+  },
+});
 
 export default withStyles(styles)(Events);
