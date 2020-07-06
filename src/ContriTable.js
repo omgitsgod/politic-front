a2e1bbeed607a5379a894f2848990a3107843d40
Ryan <omgitsgod@gmail.com>
2020-07-06 13:17:01 -0400

    Refactor and fix formatting

--- src/ContriTable.js
+++ src/ContriTable.js
@@ -1,53 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-
-const styles = theme => ({
-  root: {
-    width: '100%',
-    marginTop: theme.spacing.unit * 3,
-    overflowX: 'auto',
-  },
-  table: {
-    minWidth: 700,
-  },
-});
-
-let id = 0;
-function createData(name, amount) {
-  id += 1;
-  return { id, name, amount };
-}
+import { Table, TableBody, TableCell, TableHead, TableRow, Paper } from '@material-ui/core/Table';
 
 
+function ContriTable(props) {
 
+  const { classes, contribs } = props;
+  const rows = contribs.map(contrib => createData(contrib['@attributes'].org_name, `$${contrib['@attributes'].total}` ));
+  let id = 0;
 
+  const createData = (name, amount) => {
+    
+    id += 1;
+    return { id, name, amount };
+  }
 
-function SimpleTable(props) {
-  const { classes } = props;
-  const rows = props.contribs.map(contrib => createData(contrib["@attributes"].org_name, `$${contrib["@attributes"].total}` ))
   return (
     <Paper className={classes.root} style={{ opacity: '.7', boxShadow: 'none'}}>
       <Table className={classes.table}>
         <TableHead>
           <TableRow>
             <TableCell>Contributor</TableCell>
-            <TableCell align="right">Amount</TableCell>
+            <TableCell align='right'>Amount</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map(row => (
             <TableRow key={row.id}>
-              <TableCell component="th" scope="row">
+              <TableCell component='th' scope='row'>
                 {row.name}
               </TableCell>
-              <TableCell align="right">{row.amount}</TableCell>
+              <TableCell align='right'>{row.amount}</TableCell>
             </TableRow>
           ))}
         </TableBody>
@@ -56,8 +40,20 @@ function SimpleTable(props) {
   );
 }
 
-SimpleTable.propTypes = {
+ContriTable.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(SimpleTable);
+const styles = theme => ({
+
+  root: {
+    width: '100%',
+    marginTop: theme.spacing(3),
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 700,
+  },
+});
+
+export default withStyles(styles)(ContriTable);
