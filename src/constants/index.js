b125d605019acee6a8177b7d749465f22b4688e1
Ryan <omgitsgod@gmail.com>
2020-07-06 16:29:33 -0400

    Delete

--- src/constants/index.js
+++
@@ -1,7 +0,0 @@
-export const API_ROOT = 'https://politic-backend.herokuapp.com';
-export const API_WS_ROOT = 'wss://politic-backend.herokuapp.com/cable?token=';
-export const HEADERS = {
-  'Content-Type': 'application/json',
-  Accept: 'application/json',
-
-};
