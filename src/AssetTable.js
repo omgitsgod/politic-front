ea10b1f7644aced475798f2ecc93cf827740fc4e
Ryan <omgitsgod@gmail.com>
2020-07-06 13:20:02 -0400

    Refactor and fix formatting

--- src/AssetTable.js
+++ src/AssetTable.js
@@ -1,55 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import { Table, TableBody, TableCell, TableHead, TableRow, Paper } from '@material-ui/core/Table';
 
-const styles = theme => ({
-  root: {
-    width: '100%',
-    marginTop: theme.spacing.unit * 3,
-    overflowX: 'auto',
-  },
-  table: {
-    minWidth: 700,
-  },
-});
 
-let id = 0;
-function createData(name, holdings_high, holdings_low) {
+function AssetTable(props) {
+
+  const { classes, assets } = props;
+  const rows = assets.map(asset => createData(asset['@attributes'].name, `$${asset['@attributes'].holdings_low}`, `$${asset['@attributes'].holdings_high}`));
+  let id = 0;
+
+  const createData = (name, holdings_high, holdings_low) => {
+  
   id += 1;
-  return { id, name, holdings_high, holdings_low};
-}
+  return { id, name, holdings_high, holdings_low };
+  }
 
-
-
-
-
-function SimpleTable(props) {
-  const { classes } = props;
-  const rows = props.assets.map(asset => createData(asset["@attributes"].name, `$${asset["@attributes"].holdings_low}`, `$${asset["@attributes"].holdings_high}` ))
   return (
     <Paper className={classes.root} style={{ opacity: '.7', boxShadow: 'none'}}>
       <Table className={classes.table}>
         <TableHead>
           <TableRow>
             <TableCell>Investment</TableCell>
-            <TableCell align="right">Holdings Low End</TableCell>
-            <TableCell align="right">Holdings High End</TableCell>
+            <TableCell align='right'>Holdings Low End</TableCell>
+            <TableCell align='right'>Holdings High End</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map(row => (
             <TableRow key={row.id}>
-              <TableCell component="th" scope="row">
+              <TableCell component='th' scope='row'>
                 {row.name}
               </TableCell>
-              <TableCell align="right">{row.holdings_low}</TableCell>
-              <TableCell align="right">{row.holdings_high}</TableCell>
+              <TableCell align='right'>{row.holdings_low}</TableCell>
+              <TableCell align='right'>{row.holdings_high}</TableCell>
             </TableRow>
           ))}
         </TableBody>
@@ -58,8 +42,21 @@ function SimpleTable(props) {
   );
 }
 
-SimpleTable.propTypes = {
+AssetTable.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(SimpleTable);
+const styles = theme => ({
+
+  root: {
+    width: '100%',
+    marginTop: theme.spacing(3),
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 700,
+  },
+});
+
+export default withStyles(styles)(AssetTable);
+
