faf0377ec56ecdda113cf3a59b64825ba508a39d
Ryan <omgitsgod@gmail.com>
2020-07-18 23:46:28 -0400

    Add display component PoliticianEvents

---
+++ src/PoliticianEvents.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Typography, Grid } from '@material-ui/core';
+import EventCard from './EventCard';
+
+
+function PoliticianEvents(props) {
+
+    const { events, gridNum} = props;
+
+    return (
+      <>
+        <Typography variant='h5' align='center'>
+          Events
+        </Typography>
+        <Grid container spacing={10}>
+          <Grid container spacing={10} justify='center'>
+            {events.map((event) => (
+              <Grid item xs={gridNum} key={event.id}>
+                {' '}
+                <EventCard event={event} />{' '}
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </>
+    );
+}
+
+export default PoliticianEvents;
