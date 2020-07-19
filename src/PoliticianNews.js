2980c33b2af907b5a9bd472c9979a5b8d4581b7e
Ryan <omgitsgod@gmail.com>
2020-07-19 13:38:24 -0400

    Add display component PoliticianNews

---
+++ src/PoliticianNews.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import ArticleCard from './MediaCard';
+
+
+function PoliticianNews(props) {
+
+    const { articles, gridNum } = props;
+
+    return (
+      <Grid container spacing={10}>
+        <Grid container spacing={10} justify='center'>
+          {articles.map(article => (
+            <Grid item xs={gridNum} key={article.title}>
+              <ArticleCard article={article} />
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+    );
+}
+
+export default PoliticianNews;
