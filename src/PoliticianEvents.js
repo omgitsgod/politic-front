e66fad237ff3d9477016f96c4fb5ead019625240
Ryan <omgitsgod@gmail.com>
2020-07-19 14:49:07 -0400

    Fix formatting

--- src/PoliticianEvents.js
+++ src/PoliticianEvents.js
@@ -5,25 +5,24 @@ import EventCard from './EventCard';
 
 function PoliticianEvents(props) {
 
-    const { events, gridNum} = props;
+  const { events, gridNum} = props;
 
-    return (
-      <>
-        <Typography variant='h5' align='center'>
-          Events
-        </Typography>
-        <Grid container spacing={10}>
-          <Grid container spacing={10} justify='center'>
-            {events.map((event) => (
-              <Grid item xs={gridNum} key={event.id}>
-                {' '}
-                <EventCard event={event} />{' '}
-              </Grid>
-            ))}
-          </Grid>
+  return (
+    <>
+      <Typography variant='h5' align='center'>
+        Events
+      </Typography>
+      <Grid container spacing={10}>
+        <Grid container spacing={10} justify='center'>
+          {events.map((event) => (
+            <Grid item xs={gridNum} key={event.id}>
+              <EventCard event={event} />
+            </Grid>
+          ))}
         </Grid>
-      </>
-    );
+      </Grid>
+    </>
+  );
 }
 
 export default PoliticianEvents;
