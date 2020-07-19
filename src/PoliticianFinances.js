b6a1676c0d05483c422a7444cd09ffbe16d1d044
Ryan <omgitsgod@gmail.com>
2020-07-19 14:48:43 -0400

    Fix formatting

--- src/PoliticianFinances.js
+++ src/PoliticianFinances.js
@@ -5,33 +5,33 @@ import AssetTable from './AssetTable';
 
 function PoliticianFinances(props) {
 
-    const { finance, cycle } = props;
+  const { finance, cycle } = props;
 
-    return (
-      <>
-        <Typography component='p' align='right'>
-          First Elected: {cycle.first_elected}
-        </Typography>
-        <Typography component='p' align='right'>
-          Cash on hand: ${cycle.cash_on_hand}
-        </Typography>
-        <Typography component='p' align='right'>
-          {finance['@attributes'].transaction_count} transactions recorded:
-        </Typography>
-        <Typography component='p' align='right'>
-          Lowest: {finance['@attributes'].tx_low} | Highest:{' '}
-          {finance['@attributes'].tx_high}
-        </Typography>
-        <Typography component='p' align='right'>
-          Networth: ${finance['@attributes'].net_low} - $
-          {finance['@attributes'].net_high}
-        </Typography>
-        <Typography variant='h5' align='center'>
-          Assets
-        </Typography>
-        <AssetTable assets={finance.assets.asset} />
-      </>
-    );
+  return (
+    <>
+      <Typography component='p' align='right'>
+        First Elected: {cycle.first_elected}
+      </Typography>
+      <Typography component='p' align='right'>
+        Cash on hand: ${cycle.cash_on_hand}
+      </Typography>
+      <Typography component='p' align='right'>
+        {finance['@attributes'].transaction_count} transactions recorded:
+      </Typography>
+      <Typography component='p' align='right'>
+        Lowest: {finance['@attributes'].tx_low} | Highest:{' '}
+        {finance['@attributes'].tx_high}
+      </Typography>
+      <Typography component='p' align='right'>
+        Networth: ${finance['@attributes'].net_low} - $
+        {finance['@attributes'].net_high}
+      </Typography>
+      <Typography variant='h5' align='center'>
+        Assets
+      </Typography>
+      <AssetTable assets={finance.assets.asset} />
+    </>
+  );
 }
 
-export default PoliticianFinances
+export default PoliticianFinances;
