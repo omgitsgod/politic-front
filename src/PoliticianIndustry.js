c3710db62becf0e4d0b69c5b35e0873becda933a
Ryan <omgitsgod@gmail.com>
2020-07-19 14:48:03 -0400

    Fix formatting

--- src/PoliticianIndustry.js
+++ src/PoliticianIndustry.js
@@ -2,17 +2,18 @@ import React from 'react';
 import { Typography } from '@material-ui/core';
 import IndustryTable from './IndusTable';
 
+
 function PoliticianIndustry(props) {
 
     const { industry } = props;
 
     return (
-      <>
-        <Typography variant='h5' align='center'>
-          By Industry
-        </Typography>
-        <IndustryTable industry={industry} />
-      </>
+        <>
+            <Typography variant='h5' align='center'>
+                By Industry
+            </Typography>
+            <IndustryTable industry={industry} />
+        </>
     );
 }
 
