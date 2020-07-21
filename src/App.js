ef99191a72e5ad1f6b71e994c9bcac772bcb5f29
Ryan <omgitsgod@gmail.com>
2020-07-21 00:22:28 -0400

    Change imports to ./ArticleContainer, PoliticianContainer, BillContainer, EventContainer

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import { Route  } from 'react-router-dom';
 import { ActionCableProvider } from 'react-actioncable-provider';
-import  HeadlinesContainer from './HeadlinesContainer';
+import ArticleContainer from './ArticleContainer';
+import PoliticianContainer from './PoliticianContainer';
+import BillContainer from './BillContainer';
+import VoteContainer from './VoteContainer';
+import EventContainer from './EventContainer';
 import TopBar from './Topbar';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
 import Chat from './Chat';
 import SavedArts from './SavedArts';
-import ThePeople from './ThePeople';
-import Bills from './Bills';
-import Votes from './Votes';
-import Events from './Events';
 import ww2_and_wash_mount from './ww2_and_wash_mount.mp4';
 
 
@@ -66,26 +66,26 @@ function App() {
         <TopBar logged={loggedIn} handleUser={handleUser}/>
           {(loggedIn) ?
             <ActionCableProvider url={`${process.env.REACT_APP_API_WS_ROOT}${user.jwt}`}>
-              <Route exact path='/events' component={Events} />
-              <Route path='/people' render={(props)=><ThePeople {...props} user={user} handleUser={handleUser} address={address} zip={zip}/>}/>
-              <Route path='/news' render={(props)=><HeadlinesContainer {...props} user={user} id={id} handleUser={handleUser}/>}/>
+              <Route exact path='/events' component={EventContainer} />
+              <Route path='/people' render={(props)=><PoliticianContainer {...props} user={user} handleUser={handleUser} address={address} zip={zip}/>}/>
+              <Route path='/news' render={(props)=><ArticleContainer {...props} user={user} id={id} handleUser={handleUser}/>}/>
               <Route path='/savedarts' render={(props)=><SavedArts {...props} user={user} handleUser={handleUser}/>}/>
               <Route path='/chat' render={(props)=><Chat {...props} user={user} handleUser={handleUser}/>}/>
-              <Route  path='/bills' component={Bills} />
-              <Route  path='/votes' component={Votes} />
-              <Route exact path='/' render={(props)=><HeadlinesContainer {...props} user={user} id={id} handleUser={handleUser}/>}/>
-              <Route path='/signin' render={(props)=><HeadlinesContainer {...props} user={user} id={id} handleUser={handleUser}/>}/>
-              <Route path='/signup' render={(props)=><HeadlinesContainer {...props} user={user} id={id} handleUser={handleUser}/>}/>
+              <Route  path='/bills' component={BillContainer} />
+              <Route  path='/votes' component={VoteContainer} />
+              <Route exact path='/' render={(props)=><ArticleContainer {...props} user={user} id={id} handleUser={handleUser}/>}/>
+              <Route path='/signin' render={(props)=><ArticleContainer {...props} user={user} id={id} handleUser={handleUser}/>}/>
+              <Route path='/signup' render={(props)=><ArticleContainer {...props} user={user} id={id} handleUser={handleUser}/>}/>
             </ActionCableProvider>
           :
-            <div>
-              <Route exact path='/events' component={Events} />
-              <Route exact path='/bills' component={Bills} />
-              <Route exact path='/votes' component={Votes} />
-              <Route exact path='/' component={HeadlinesContainer} />
+            <>
+              <Route exact path='/events' component={EventContainer} />
+              <Route exact path='/bills' component={BillContainer} />
+              <Route exact path='/votes' component={VoteContainer} />
+              <Route exact path='/' component={ArticleContainer} />
               <Route path='/signin' render={(props)=><SignIn {...props} handleUser={handleUser}/>}/>
               <Route path='/signup' render={(props)=><SignUp {...props} handleUser={handleUser}/>}/>
-            </div>
+            </>
           }
       </div>
   );
