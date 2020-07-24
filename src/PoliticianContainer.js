26289a87332749570da17dfb48352f3406e57500
Ryan <omgitsgod@gmail.com>
2020-07-23 23:39:11 -0400

    Add ability to open ZipCodeModal

--- src/PoliticianContainer.js
+++ src/PoliticianContainer.js
@@ -2,14 +2,17 @@ import React, { useState, useEffect } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import PoliticianList from './PoliticianList';
 import Politician from './Politician';
+import ZipCodeModal from './ZipCodeModal';
 import { isBrowser } from 'react-device-detect';
 
 
 function PoliticanContainer(props) {
   
   const [feds, setFeds] = useState([]);
+  const [openZip, setOpenZip] = useState(true);
+  const [zip, setZip] = useState();
   const [fed, setFed] = useState('');
-  const { classes, address } = props;
+  const { classes } = props;
   const gridNum = isBrowser ? 3 : 12;
 
   const handlePol = (pol) => {
@@ -17,11 +20,16 @@ function PoliticanContainer(props) {
     console.log(pol)
     pol === 'Back' ? setFed('') : setFed(pol);
   }
+  
+  const handleZipOpen = () => {
+    
+    setOpenZip(true);
+  }
 
   const fetchReps = async () => {
 
-    const zip = address || 20001;
-    const json = await fetch(`${process.env.REACT_APP_BACK_HOST}/reps/${zip}`).then(r => r.json());
+    const actionZip = zip || 10005;
+    const json = await fetch(`${process.env.REACT_APP_BACK_HOST}/reps/${actionZip}`).then(r => r.json());
     const officials = json.officials;
     const state = json.normalizedInput.state;
     
@@ -30,19 +38,23 @@ function PoliticanContainer(props) {
     console.log(state)
   }
 
-
   useEffect(() => {
 
     fetchReps();
-  }, []);
+  }, [zip]);
   
   return (
     <main className={classes.main}>
-      {fed.length === 0 ? (
+      {zip && fed.length === 0 && feds.length > 0 ? 
         <PoliticianList feds={feds} gridNum={gridNum} handlePol={handlePol} />
-      ) : (
-        <Politician fed={fed} handlePol={handlePol} data={fed.data} />
-      )}
+      : 
+        <ZipCodeModal open={openZip} setOpen={setOpenZip} setZip={setZip} fetchReps={fetchReps} />
+      }
+      {fed.length > 0  ? 
+        <Politician fed={fed} handlePol={handlePol} data={fed.data} /> 
+      : 
+        null
+      }
     </main>
   );
 }
