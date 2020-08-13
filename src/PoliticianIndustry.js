9a7366f8d01eafe6aedbb030fc5948a5eb055f35
Ryan <omgitsgod@gmail.com>
2020-08-12 22:06:45 -0400

    Change fragment to Paper

--- src/PoliticianIndustry.js
+++ src/PoliticianIndustry.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+import { Typography, Paper } from '@material-ui/core';
 import IndustryTable from './IndustryTable';
 
 
 function PoliticianIndustry(props) {
 
-    const { industry } = props;
+  const { industry } = props;
 
-    return (
-        <>
-            <Typography variant='h5' align='center'>
-                By Industry
-            </Typography>
-            <IndustryTable industry={industry} />
-        </>
-    );
+  return (
+    <Paper style={{background: 'transparent', boxShadow: 'none', width: '100%'}}>
+      <Typography variant='h5' align='center'>
+        By Industry
+      </Typography>
+      <IndustryTable industry={industry} />
+    </Paper>
+  );
 }
 
 export default PoliticianIndustry;
