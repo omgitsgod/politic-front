dff46a134b4ebb0a66c06663c88fd802f75a1746
Ryan <omgitsgod@gmail.com>
2020-08-12 22:08:57 -0400

    Change fragment to Paper

--- src/PoliticianContributions.js
+++ src/PoliticianContributions.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+import { Typography, Paper } from '@material-ui/core';
 import ContributionTable from './ContributionTable';
 
 
 function PoliticianContributions(props) {
 
-    const { contributions } = props;
+  const { contributions, classes } = props;
 
-    return (
-        <>
-            <Typography variant='h5' align='center'>
-                Top Contributors
-            </Typography>
-            <ContributionTable contribs={contributions} />
-        </>
-    );
+  return (
+    <Paper style={{background: 'transparent', boxShadow: 'none', width: '100%'}}>
+      <Typography variant='h5' align='center'>
+        Top Contributors
+      </Typography>
+      <ContributionTable contribs={contributions} />
+    </Paper>
+  );
 }
 
 export default PoliticianContributions;
