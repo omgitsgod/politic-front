fa5354578c6aa4a5873e2faac7aaf63a84636b87
Ryan <omgitsgod@gmail.com>
2020-08-12 22:09:38 -0400

    Change fragment to Paper

--- src/PoliticianBills.js
+++ src/PoliticianBills.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Typography, Grid } from '@material-ui/core';
+import { Typography, Grid, Paper } from '@material-ui/core';
 import BillCard from './BillCard';
 
 
@@ -8,20 +8,20 @@ function PoliticianBills(props) {
   const { bills, gridNum, handleBillPol } = props;
 
   return (
-    <>
+    <Paper style={{background: 'transparent', boxShadow: 'none', width: '100%'}}>
       <Typography variant='h5' align='center'>
         Bills
       </Typography>
       <Grid container spacing={10}>
         <Grid container spacing={10} justify='center'>
           {bills.map(bill => (
-            <Grid item xs={gridNum} key={bill.id}>
+            <Grid item xs={gridNum} key={bill.bill_id}>
               <BillCard bill={bill} handlePol={handleBillPol} />
             </Grid>
           ))}
         </Grid>
       </Grid>
-    </>
+    </Paper>
   );
 }
 
