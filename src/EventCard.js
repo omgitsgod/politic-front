c95bd2e64ac572004fbcfcd9b917496a38408412
Ryan <omgitsgod@gmail.com>
2020-09-03 22:58:33 -0400

    Add border to EventCard

--- src/EventCard.js
+++ src/EventCard.js
@@ -61,7 +61,8 @@ const styles = (theme) => ({
 
   card: {
     maxWidth: 345,
-    marginTop: theme.spacing(8)
+    marginTop: theme.spacing(8),
+    border: '2px solid #000',
   },
   media: {
     height: 200,
