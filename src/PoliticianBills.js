8199fbd30d2885130c763e84934472e4412ac4c8
Ryan <omgitsgod@gmail.com>
2020-09-24 11:20:39 -0400

    Remove gridNum and add breakpoints to grid

--- src/PoliticianBills.js
+++ src/PoliticianBills.js
@@ -5,21 +5,19 @@ import BillCard from './BillCard';
 
 function PoliticianBills(props) {
 
-  const { bills, gridNum, handleBillPol } = props;
+  const { bills, handleBillPol } = props;
 
   return (
     <Paper style={{background: 'transparent', boxShadow: 'none', width: '100%'}}>
       <Typography variant='h5' align='center'>
         Bills
       </Typography>
-      <Grid container spacing={10}>
-        <Grid container spacing={10} justify='center'>
-          {bills.map(bill => (
-            <Grid item xs={gridNum} key={bill.bill_id}>
-              <BillCard bill={bill} handlePol={handleBillPol} />
-            </Grid>
-          ))}
-        </Grid>
+      <Grid container spacing={10} justify='center'>
+        {bills.map(bill => (
+          <Grid item xs={12} sm={6} md={3}  key={bill.bill_id}>
+            <BillCard bill={bill} handlePol={handleBillPol} />
+          </Grid>
+        ))}
       </Grid>
     </Paper>
   );
